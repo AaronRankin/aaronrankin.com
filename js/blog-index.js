1363205307a3770c496e4d985d1d91eaa917bee7
AaronRankin <62940304+AaronRankin@users.noreply.github.com>
2020-04-09 20:11:42 +0100

    Update blog-index.js

--- js/blog-index.js
+++ js/blog-index.js
@@ -36,7 +36,7 @@ xhttp.onreadystatechange = function() {
             // get the anchor link buttons
             const menuBtn = $(nextButton);
               // when each button is clicked
-            menuBtn.click(()=>{	
+            menuBtn.click(()=>{
               // set a short timeout before taking action
               // so as to allow hash to be set
             setTimeout(()=>{
@@ -49,7 +49,7 @@ xhttp.onreadystatechange = function() {
           history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
         }
 
-      };
+      });
 
       backButton.addEventListener("click",pageDown);
 
