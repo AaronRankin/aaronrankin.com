1dd7192fa6326ab6e8f2664845c4e7cbc7f08c4c
AaronRankin <62940304+AaronRankin@users.noreply.github.com>
2020-04-09 20:10:13 +0100

    Update blog-index.js

--- js/blog-index.js
+++ js/blog-index.js
@@ -32,6 +32,19 @@ xhttp.onreadystatechange = function() {
 
         };
 
+        $(document).ready(function() {
+            // get the anchor link buttons
+            const menuBtn = $(nextButton);
+              // when each button is clicked
+            menuBtn.click(()=>{	
+              // set a short timeout before taking action
+              // so as to allow hash to be set
+            setTimeout(()=>{
+              // call removeHash function after set timeout
+              removeHash();
+            }, 5); // 5 millisecond timeout in this case
+        });
+
         function removeHash(){
           history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
         }
